142cc24f7d0aa709255d39d641f7f1d956c7a457
Petra Avramovic <petra.avramovic@gmail.com>
2020-06-21 16:46:02 +0200

    Update TweetManager.py

--- GetOldTweets3/manager/TweetManager.py
+++ GetOldTweets3/manager/TweetManager.py
@@ -128,7 +128,7 @@ class TweetManager:
                     if tweetCriteria.maxTweets > 0 and batch_cnt_results >= tweetCriteria.maxTweets:
                         active = False
                         break
-                time.sleep(5)
+                #time.sleep(5)
 
             if receiveBuffer and len(resultsAux) > 0:
                 receiveBuffer(resultsAux)
