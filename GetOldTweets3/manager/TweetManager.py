be535e3f21b68c53f4cf427a1b55ff9b5a745339
Petra Avramovic <petra.avramovic@gmail.com>
2020-10-10 19:09:45 +0200

    Update TweetManager.py

--- GetOldTweets3/manager/TweetManager.py
+++ GetOldTweets3/manager/TweetManager.py
@@ -328,12 +328,12 @@ class TweetManager:
         """Invoke an HTTP query to Twitter.
         Should not be used as an API function. A static method.
         """
-        url = "https://twitter.com/i/search/timeline?"
+        url = "https://twitter.com/i/search?"
 
         if not tweetCriteria.topTweets:
             url += "f=tweets&"
 
-        url += ("vertical=news&q=%s&src=typed_query&%s"
+        url += ("vertical=news&q=%s&src=typd&%s"
                 "&include_available_features=1&include_entities=1&max_position=%s"
                 "&reset_error_state=false")
 
